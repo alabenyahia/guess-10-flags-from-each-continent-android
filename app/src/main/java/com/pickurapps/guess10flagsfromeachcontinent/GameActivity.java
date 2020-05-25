5739c8bbf50d31e2baf0e6356cff991dc70ef15b
ala <alabny18@gmail.com>
2020-05-26 00:26:34 +0100

    Add answerchar clicked code

--- app/src/main/java/com/pickurapps/guess10flagsfromeachcontinent/GameActivity.java
+++ app/src/main/java/com/pickurapps/guess10flagsfromeachcontinent/GameActivity.java
@@ -16,6 +16,8 @@ public class GameActivity extends AppCompatActivity {
     TextView coinsNumTv;
     UserData userData;
     char[] writtenAnswer = {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '};
+    String[] tags = {"", "", "", "", "", "", "", "", "", "", "", "", "", ""};
+    int tagsNextPos;
     int writtenAnswerNextPos;
 
     @Override
@@ -43,7 +45,7 @@ public class GameActivity extends AppCompatActivity {
         flagIv.setImageResource(userData.getCurrentContinent().getStages()[userData.getCurrentContinent().getCurrentFlagNum()].
                                         getImageId());
 
-        // set answer textviews
+        // set answer textviews first line
         LinearLayout answerFl = findViewById(R.id.answer_fl);
         boolean done = false;
         for (int i=0; i< answerFl.getChildCount(); i++) {
@@ -59,6 +61,7 @@ public class GameActivity extends AppCompatActivity {
             }
         }
 
+        // set answer textviews second line
         LinearLayout answerSl = findViewById(R.id.answer_sl);
         done = false;
         if (userData.getCurrentContinent().getStages()[userData.getCurrentContinent().getCurrentFlagNum()].getCorrectAnswer().length > 6) {
@@ -76,6 +79,7 @@ public class GameActivity extends AppCompatActivity {
             }
         }
 
+        // if correct answer is less than 6 then delete answer second line
         if (userData.getCurrentContinent().getStages()[userData.getCurrentContinent().getCurrentFlagNum()].getCorrectAnswer().length <= 6) {
             answerSl.setVisibility(View.GONE);
         }
@@ -169,6 +173,7 @@ public class GameActivity extends AppCompatActivity {
         for (int i=0; i< answerFl.getChildCount(); i++) {
             TextView tv = (TextView) answerFl.getChildAt(i);
             tv.setText(String.valueOf(writtenAnswer[i]));
+            tv.setTag(tags[i]);
 
         }
 
@@ -176,18 +181,101 @@ public class GameActivity extends AppCompatActivity {
         for (int i=0,j=answerFl.getChildCount(); i< answerSl.getChildCount(); i++,j++) {
             TextView tv = (TextView) answerSl.getChildAt(i);
             tv.setText(String.valueOf(writtenAnswer[j]));
+            tv.setTag(tags[j]);
         }
     }
 
     // click on a random character textview
     private void randomCharClicked(View view){
-        if (writtenAnswerNextPos < userData.getCurrentContinent().getStages()[userData
+        int nextPos = searchWrittenAnswerNextPos();
+        if (nextPos != -1 && nextPos < userData.getCurrentContinent().getStages()[userData
                 .getCurrentContinent().getCurrentFlagNum()].getCorrectAnswer().length) {
-            writtenAnswer[writtenAnswerNextPos] = ((TextView)view).getText().toString().charAt(0);
-            writtenAnswerNextPos++;
+            writtenAnswer[nextPos] = ((TextView)view).getText().toString().charAt(0);
+            tags[nextPos] = ((TextView)view).getTag().toString();
+            /*tagsNextPos++;
+            writtenAnswerNextPos++;*/
             fillAnswerTextViews();
             view.setVisibility(View.INVISIBLE);
         }
 
+    }
+
+    public void a1(View view) {
+        answerCharClicked(view, 0);
+    }
+
+    public void a2(View view) {
+        answerCharClicked(view, 1);
+    }
+
+    public void a3(View view) {
+        answerCharClicked(view, 2);
+    }
+
+    public void a4(View view) {
+        answerCharClicked(view, 3);
+    }
+
+    public void a5(View view) {
+        answerCharClicked(view, 4);
+    }
+
+    public void a6(View view) {
+        answerCharClicked(view, 5);
+    }
+
+    public void a7(View view) {
+        answerCharClicked(view, 6);
+    }
+
+    public void a8(View view) {
+        answerCharClicked(view, 7);
+    }
+
+    public void a9(View view) {
+        answerCharClicked(view, 8);
+    }
+
+    public void a10(View view) {
+        answerCharClicked(view, 9);
+    }
+
+    public void a11(View view) {
+        answerCharClicked(view, 10);
+    }
+
+    public void a12(View view) {
+        answerCharClicked(view, 11);
+    }
+
+    // fired when answer char clicked
+    private void answerCharClicked(View view, int pos) {
+        ((TextView)view).setText(" ");
+        writtenAnswer[pos] = ' ';
+        tags[pos] = "";
+
+        LinearLayout pickFl = findViewById(R.id.pick_fl);
+        for (int i=0; i < pickFl.getChildCount(); i++) {
+            TextView tv = (TextView) pickFl.getChildAt(i);
+            if (((TextView)view).getTag().equals(tv.getTag())) {
+                tv.setVisibility(View.VISIBLE);
+            }
+        }
+
+        LinearLayout pickSl = findViewById(R.id.pick_sl);
+        for (int i=0, j=7; i < pickFl.getChildCount(); i++, j++) {
+            TextView tv = (TextView) pickSl.getChildAt(i);
+            if (((TextView)view).getTag().equals(tv.getTag())) {
+                tv.setVisibility(View.VISIBLE);
+            }
+        }
+    }
+
+    // search for the next position to use in the written answer array
+    private int searchWrittenAnswerNextPos() {
+        for (int i=0; i<writtenAnswer.length ; i++) {
+            if (writtenAnswer[i] == ' ') return i;
+        }
+        return -1;
     }
 }
