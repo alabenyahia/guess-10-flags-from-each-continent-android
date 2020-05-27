b7077be3ef3d64cb83785c29a9afcc9a29f93b66
ala <alabny18@gmail.com>
2020-05-27 06:37:38 +0100

    Remove answer textviews to a single textview

--- app/src/main/java/com/pickurapps/guess10flagsfromeachcontinent/GameActivity.java
+++ app/src/main/java/com/pickurapps/guess10flagsfromeachcontinent/GameActivity.java
@@ -7,10 +7,8 @@ import android.app.Dialog;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
@@ -67,46 +65,9 @@ public class GameActivity extends AppCompatActivity {
         ImageView flagIv = findViewById(R.id.flag_iv);
         flagIv.setImageResource(userData.getCurrentContinent().getStages()[userData.getCurrentContinent().getCurrentFlagNum()].
                 getImageId());
-
-        // set answer textviews first line
-        LinearLayout answerFl = findViewById(R.id.answer_fl);
-        boolean done = false;
-        for (int i=0; i< answerFl.getChildCount(); i++) {
-            if(userData.getCurrentContinent().getStages()[userData.getCurrentContinent().getCurrentFlagNum()].getCorrectAnswer().length<=i) {
-                done = true;
-            }
-            TextView tv = (TextView) answerFl.getChildAt(i);
-            tv.setVisibility(View.VISIBLE);
-            if (!done) {
-                tv.setText("");
-            }else{
-                tv.setVisibility(View.GONE);
-            }
-        }
-
-        // set answer textviews second line
-        LinearLayout answerSl = findViewById(R.id.answer_sl);
-        done = false;
-        if (userData.getCurrentContinent().getStages()[userData.getCurrentContinent().getCurrentFlagNum()].getCorrectAnswer().length > 6) {
-            for (int i=0, j=6; i< answerSl.getChildCount(); i++, j++) {
-                if(userData.getCurrentContinent().getStages()[userData.getCurrentContinent().getCurrentFlagNum()].getCorrectAnswer().length<=j) {
-                    done = true;
-                }
-                TextView tv = (TextView) answerSl.getChildAt(i);
-                tv.setVisibility(View.VISIBLE);
-                if (!done) {
-                    tv.setText("");
-                }else{
-                    tv.setVisibility(View.GONE);
-                }
-            }
-        }
-
-        // if correct answer is less than 6 then delete answer second line
-        if (userData.getCurrentContinent().getStages()[userData.getCurrentContinent().getCurrentFlagNum()].getCorrectAnswer().length <= 6) {
-            answerSl.setVisibility(View.GONE);
-        }
-
+        //set answer textview to empty string
+        TextView answerTv = findViewById(R.id.answer_tv);
+        answerTv.setText("");
         // set random chars textviews
         LinearLayout pickFl = findViewById(R.id.pick_fl);
         for (int i=0; i < pickFl.getChildCount(); i++) {
@@ -191,22 +152,14 @@ public class GameActivity extends AppCompatActivity {
         randomCharClicked(view);
     }
 
-    // fill the answer textviews with the chosen chars
-    private void fillAnswerTextViews() {
-        LinearLayout answerFl = findViewById(R.id.answer_fl);
-        for (int i=0; i< answerFl.getChildCount(); i++) {
-            TextView tv = (TextView) answerFl.getChildAt(i);
-            tv.setText(String.valueOf(writtenAnswer[i]));
-            tv.setTag(tags[i]);
-
+    // fill the answer textview with the chosen chars
+    private void fillAnswerTextView() {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i=0; i<writtenAnswer.length;i++) {
+            stringBuilder.append(writtenAnswer[i]);
         }
-
-        LinearLayout answerSl = findViewById(R.id.answer_sl);
-        for (int i=0,j=answerFl.getChildCount(); i< answerSl.getChildCount(); i++,j++) {
-            TextView tv = (TextView) answerSl.getChildAt(i);
-            tv.setText(String.valueOf(writtenAnswer[j]));
-            tv.setTag(tags[j]);
-        }
+        TextView tv = findViewById(R.id.answer_tv);
+        tv.setText(stringBuilder.toString());
     }
 
     // click on a random character textview
@@ -215,12 +168,13 @@ public class GameActivity extends AppCompatActivity {
         if (nextPos == -1) {
             Toast.makeText(view.getContext(), "Wrong Answer",
                     Toast.LENGTH_SHORT).show();
+            setupNextFlag();
         }
         else if (nextPos < userData.getCurrentContinent().getStages()[userData
                 .getCurrentContinent().getCurrentFlagNum()].getCorrectAnswer().length) {
             writtenAnswer[nextPos] = ((TextView)view).getText().toString().charAt(0);
             tags[nextPos] = view.getTag().toString();
-            fillAnswerTextViews();
+            fillAnswerTextView();
             view.setVisibility(View.INVISIBLE);
             nextPos = searchWrittenAnswerNextPos();
             if (isFlagCompleted()) {
@@ -331,54 +285,6 @@ public class GameActivity extends AppCompatActivity {
         }
 
 
-    }
-
-    public void a1(View view) {
-        answerCharClicked(view, 0);
-    }
-
-    public void a2(View view) {
-        answerCharClicked(view, 1);
-    }
-
-    public void a3(View view) {
-        answerCharClicked(view, 2);
-    }
-
-    public void a4(View view) {
-        answerCharClicked(view, 3);
-    }
-
-    public void a5(View view) {
-        answerCharClicked(view, 4);
-    }
-
-    public void a6(View view) {
-        answerCharClicked(view, 5);
-    }
-
-    public void a7(View view) {
-        answerCharClicked(view, 6);
-    }
-
-    public void a8(View view) {
-        answerCharClicked(view, 7);
-    }
-
-    public void a9(View view) {
-        answerCharClicked(view, 8);
-    }
-
-    public void a10(View view) {
-        answerCharClicked(view, 9);
-    }
-
-    public void a11(View view) {
-        answerCharClicked(view, 10);
-    }
-
-    public void a12(View view) {
-        answerCharClicked(view, 11);
     }
 
     // fired when answer char clicked
